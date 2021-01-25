e807d9eb522da4d11fc196561bd1c9e3504eec9f
Laura <keenlaura@hotmail.co.uk>
2021-01-25 12:37:33 +0000

    Completed program and tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,28 @@
+# def fizzbuzz(number)
+#   number = 1
+
+#   while number <= 100 do
+#   #(1..number).each do |num|
+#     if number % 3 == 0 && number % 5 == 0
+#       number = number + 1
+#       return "fizzbuzz"
+#     elsif number % 3 == 0
+#       number = number + 1
+#       return "fizz"
+#     elsif number % 5 == 0
+#       number = number + 1
+#       return "buzz"
+#     else
+#       number
+#     end
+#   end
+# end
+
 def fizzbuzz(number)
-  'fizz'
+  case
+    when number % 3 == 0 && number % 5 == 0 then "fizzbuzz"
+    when number % 3 == 0 then "fizz"
+    when number % 5 == 0 then "buzz"
+    else number
+  end
 end
